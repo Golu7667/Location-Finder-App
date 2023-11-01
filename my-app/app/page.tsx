0b40a2fb220a5d0135508bebdfc24ffdd44f37b3
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-01 08:55:04 +0000

    add location allow and show

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+// components/Location.js
+'use client'
+import { useState, useEffect } from 'react';
 
-export default function Home() {
+function DynamicContent() {
+  const [data, setData] = useState(null);
+   
+  useEffect(() => {
+    // Fetch data from an API or perform other side effects
+    fetch('https://api.example.com/data')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []); // Empty dependency array means the effect runs once after the initial render
+
   return (
-    <>
-    Home
-    </>
-  )
+    <div>
+      {data ? <p>Data: {data}</p> : <p>Loading data...</p>}
+    </div>
+  );
 }
+
+export default DynamicContent;
