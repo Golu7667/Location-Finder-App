dbe83ce2a02da721950cb5deb004cc23d3d5d180
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-01 12:21:45 +0000

    set value zero

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -5,8 +5,8 @@ import { useState, useEffect } from 'react';
 
 function DynamicContent() {
   const [isButton,setIsButton]=useState(false) 
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
 
   useEffect(() => {
     // Check if geolocation is available in the browser
