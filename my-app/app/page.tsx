ae6504ba97b6823e60f16fbde863a86b94bb3103
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-01 12:14:09 +0000

    final done location app

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -29,21 +29,17 @@ function DynamicContent() {
    }
 
   return (
-    <div className='min-h-screen w-full bg-neutral-200  '>
+    <div className='min-h-screen w-full   '>
       <div className=' h-8 font-extrabold  flex justify-center item-center pt-16' >Welcome To Find Your Location</div>
      <div className='flex justify-center item-center mt-8'>
-      <div className='w-1/2 h-96 rounded-2xl shadow-2xl shadow-indigo-500/50 bg-blue-100 '>
-         <div className='w-full h-36 flex  justify-around pt-8'>
+      <div className='w-1/2 h-96 rounded-2xl shadow-xl shadow-gray-700 bg-slate-50  border-t-4'>
+         <div className='w-full h-1/2 flex  justify-around pt-8'>
          <img className='w-32 h-32' src="/location.svg"/>
          <button  className='w-52 h-6 font-extrabold bg-sky-500 text-white rounded-xl hover:bg-sky-700 ' onClick={()=>handle()}>Find Your Loacation </button>
          </div>
-         <div className='w-full h-36'>
-        {<div className='font-extrabold'>
-          <p >Latitude: {latitude}</p>
-          <p>Longitude: {longitude}</p>
-        
-        </div>
-      }
+         <div className='font-extrabold h-1/2 flex justify-center item-center mt-16'>
+          <p className='w-1/2 mx-8'>Latitude: <text className='bg-white'>{latitude}</text> </p>
+          <p className='w-1/2 mx-8'>Longitude: <text>{longitude}</text></p>
       </div>
       </div>
       </div>
